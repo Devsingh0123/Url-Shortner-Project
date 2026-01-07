ee4c63cfe58683213b9540928a030c592ba00a4d
Devsingh0123 <dayamns20@gmail.com>
2026-01-07 18:30:32 +0530

    make basic UI AND taste it

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import urlRouter from "./routes/url.route.js";
+import cors from "cors";
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  }));
 
 app.use(express.json());
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios"
 
 const App = () => {
-  return (
-    <div className='bg-red-600'>App</div>
-  )
-}
+  const [originalUrl, setOriginalUrl] = useState("");
 
-export default App
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      console.log(originalUrl);
+      
+
+      const res = await axios.post("http://localhost:8000/api/shortener", {
+        originalUrl,
+      } );
+      console.log(res.data.data.shortUrl);
+      
+      setOriginalUrl("");
+    } catch (error) {
+       console.error("Error while creating url",error)
+
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col sm:flex-row w-full max-w-xl gap-2 mb-6"
+    >
+      <input
+        type="text"
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+        placeholder="Enter URL"
+        className="flex-1 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
+        
+      >click</button>
+    </form>
+  );
+};
+
+export default App;
